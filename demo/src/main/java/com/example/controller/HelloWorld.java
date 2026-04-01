45807ab884cc7a7b7f978adf9acadc81fc1cbb7b
Minokainduwara <minokainduwara@gmail.com>
2026-04-01 16:19:32 +0530

    Feat: Quick Start

--- demo/src/main/java/com/example/controller/HelloWorld.java
+++ demo/src/main/java/com/example/controller/HelloWorld.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DemoController {
-    @GetMapping("/hello")
-    public String HelloWorld() {
+public class HelloWorld {
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
         return "Hello World";
     }
 }
