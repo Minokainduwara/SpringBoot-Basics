7cccba4d78d9c60c6e12d7e24e6426fd588d46f8
Minokainduwara <minokainduwara@gmail.com>
2026-04-08 19:43:16 +0530

    Feat: Field Injection Auto Wiring

--- demo/src/main/java/com/example/dev/Dev.java
+++ demo/src/main/java/com/example/dev/Dev.java
@@ -1,10 +1,14 @@
 package com.example.dev;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+    @Autowired
+    private laptop lp;
     public void build(){
+        lp.compile();
         System.out.println("Dev build");
     }
 }
