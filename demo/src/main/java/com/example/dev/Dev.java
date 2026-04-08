b76552dc9ef01b61a7956917907148e2e74c9199
Minokainduwara <minokainduwara@gmail.com>
2026-04-08 10:17:44 +0530

    Feat: Dependency Injection

---
+++ demo/src/main/java/com/example/dev/Dev.java
@@ -0,0 +1,10 @@
+package com.example.dev;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("Dev build");
+    }
+}
